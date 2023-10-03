d3b1597dcc24fd0e2fbb102276f46d9410bef4ad
jareeri <abdojareeri2000@gmail.com>
2023-10-03 23:12:09 +0300

    using dom and events

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-// var x = 5;
-// var z = 3;
-// console.log(x + z);
-// var name = prompt("Please enter your name:", "Abdullah");
+var x = 5;
+var z = 3;
+console.log(x + z);
+var name = prompt("Please enter your name:", "Abdullah");
 
-// // if (gender === "male") {
-// //     alert(`Welcome, Mr ${name}.`);
-// //   } else if (gender === "female") {
-// //     alert(`Welcome, Ms ${name}.`);
-// //   } else {
-// //     alert(`Welcome, ${name}`);
-// //   }
-// var gender
-// yourGender();
 // if (gender === "male") {
-//   alert(`Welcome, Mr ${name}.`);
-// } else alert(`Welcome, Ms ${name}.`);
-
-// // Ask the user if they want a hot or cold drink
-// const drinkType = prompt("Do you want a hot or cold drink?");
-
-// // Ask the user for the name of their drink
-// const drinkName = prompt("Enter the name of your drink:");
-
-// // Alert the user that their drink is getting prepared
-// alert(`Your ${drinkType} ${drinkName} is getting prepared.`);
-
-// // Print a statement on the console that shows the user's name and drink order
-// console.log(`${name} ordered a ${drinkType} ${drinkName}.`);
-// let array = [name , gender , drinkType , drinkName];
-// for (let index = 0; index < array.length; index++) {
-//   console.log( array[index]);
-
-// }
-// function yourGender() {
-//   while (gender  !== "male" && gender !== "female") {
-//      gender = prompt("Please enter your gender: male or female", "male");
+//     alert(`Welcome, Mr ${name}.`);
+//   } else if (gender === "female") {
+//     alert(`Welcome, Ms ${name}.`);
+//   } else {
+//     alert(`Welcome, ${name}`);
 //   }
-//   // eeeee
-// }
+var gender
+yourGender();
+if (gender === "male") {
+  alert(`Welcome, Mr ${name}.`);
+} else alert(`Welcome, Ms ${name}.`);
+
+// Ask the user if they want a hot or cold drink
+const drinkType = prompt("Do you want a hot or cold drink?");
+
+// Ask the user for the name of their drink
+const drinkName = prompt("Enter the name of your drink:");
+
+// Alert the user that their drink is getting prepared
+alert(`Your ${drinkType} ${drinkName} is getting prepared.`);
+
+// Print a statement on the console that shows the user's name and drink order
+console.log(`${name} ordered a ${drinkType} ${drinkName}.`);
+let array = [name , gender , drinkType , drinkName];
+for (let index = 0; index < array.length; index++) {
+  console.log( array[index]);
+
+}
+function yourGender() {
+  while (gender  !== "male" && gender !== "female") {
+     gender = prompt("Please enter your gender: male or female", "male");
+  }
+  // eeeee
+}
 let div_1 = document.createElement("div");
 
 document.getElementById("myDIV").appendChild(div_1);
